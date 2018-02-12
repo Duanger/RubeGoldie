60c88a09edfb1bb1b81c491030b1c009f5ddcc7e
Duanger <aaronshenglu@gmail.com>
2018-02-12 00:55:22 -0500

    added font and tube not working >o<

---
+++ RubeGoldie/Assets/BrainBehaviour.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BrainBehaviour : MonoBehaviour
+{
+
+	public AudioSource brainSplooge;
+	void Start ()
+	{
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	private void OnCollisionEnter(Collision other)
+	{
+		brainSplooge.PlayOneShot(brainSplooge.clip);
+	}
+}
